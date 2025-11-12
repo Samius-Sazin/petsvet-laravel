8c590a3b0e785523f093a06da0bff0756251a7c6
sabuj494 <sabuj494k@gmail.com>
2025-11-12 15:48:58 +0600

    about page init

---
+++ .resources/views/sabuj-partials/header.blade.php
@@ -0,0 +1,20 @@
+<header class="sabuj-header">
+    <div class="sabuj-header-container">
+        <div class="sabuj-logo">
+            <a href="/">PetsVEt</a>
+        </div>
+     <div class="sabuj-header-actions">
+        <nav class="sabuj-nav">
+            <ul class="sabuj-nav-list">
+                <li><a href="/" class="sabuj-nav-link">Home</a></li>
+                <li><a href="/about" class="sabuj-nav-link {{ request()->is('about') ? 'active' : '' }}">About Us</a></li>
+                <li><a href="/blog" class="sabuj-nav-link">Blogs</a></li>
+                <li><a href="/community" class="sabuj-nav-link">Consultancy</a></li>
+                <li><a href="/qna" class="sabuj-nav-link">Q & A</a></li>
+                <li><a href="/products" class="sabuj-nav-link">Shop</a></li>
+            </ul>
+         </nav>
+        <a href="#" class="sabuj-btn-login">Log In</a>
+     </div>
+    </div>
+</header>
