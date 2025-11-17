12f98d87f707a35af83746ffe9cfe78108a1ada9
Samius Sazin <samiussazin45@gmail.com>
2025-11-18 00:57:27 +0600

    Update project name

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -1,11 +1,15 @@
+@php
+    $totalProducts = 42;
+@endphp
+
 @extends('main')
 
 @section('title', '| Products')
 
 @section('content')
-<div class="container text-center my-5">
-    <h1 class="display-4">Products Page</h1>
-    <p class="lead">This is a dummy home page for route checking.</p>
-    <a href="{{ route('home') }}" class="btn btn-primary mt-3">Refresh Page</a>
-</div>
+    <div class="container text-center my-5">
+        <h1 class="display-4">Products Page</h1>
+        <p class="lead">Total Products: {{ $totalProducts }}</p>
+        <a href="{{ route('home') }}" class="btn btn-primary mt-3">Refresh Page</a>
+    </div>
 @endsection
