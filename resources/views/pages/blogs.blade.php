e74bc6112c7dd0f5b92c38ba5c523d92dd75babf
sabuj494 <sabuj494k@gmail.com>
2025-11-20 23:43:53 +0600

    create blogs page and add blog images

--- resources/views/pages/blogs.blade.php
+++ resources/views/pages/blogs.blade.php
@@ -3,9 +3,228 @@
 @section('title', '| Blogs')
 
 @section('content')
-<div class="container text-center my-5">
-    <h1 class="display-4">Blog Page</h1>
-    <p class="lead">This is a dummy home page for route checking.</p>
-    <a href="{{ route('home') }}" class="btn btn-primary mt-3">Refresh Page</a>
+
+
+<div class="container-fluid px-0 mb-5">
+    <div class="position-relative">
+        <img src="/images/blogs/hero.jpg" alt="Blogs Banner" 
+             class="w-100" style="height: 400px; object-fit: cover;">
+        <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center" style="background-color: rgba(0, 0, 0, 0.6);">
+            <h1 class="text-white display-3 fw-bold">Blogs</h1>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container mb-5">
+    <div class="row g-4">
+      
+        <div class="col-lg-4 col-md-6">
+            <div class="card h-100 border-0 ">
+                <img src="/images/blogs/blog-1.jpg" 
+                     class="card-img-top" alt="Blog Post" style="height: 250px; object-fit: cover;">
+                <div class="card-body">
+                    <p class="text-primary small mb-2 fw-semibold">Pet Health</p>
+                    <h5 class="card-title mb-3">Understanding Your Pet's Body Language: Signs of Illness</h5>
+                    <p class="card-text text-muted small mb-3">
+                        Lorem ipsum dolor sit amet consectetur. Arcu magna nam nibh ultricies. 
+                        Duis at congue ultrices at. Praesent risus risus tortor.
+                    </p>
+                    <p class="text-muted small mb-3">12 Oct, 2024</p>
+                    <a href="#" class="text-decoration-none d-inline-flex align-items-center">
+                        Read More 
+                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="ms-2">
+                            <path d="M1.25 8.25H15.25M15.25 8.25L8.25 1.25M15.25 8.25L8.25 15.25" stroke="#0082CF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+            <div class="card h-100 border-0 ">
+                <img src="/images/blogs/blog-2.jpg" 
+                     class="card-img-top" alt="Blog Post" style="height: 250px; object-fit: cover;">
+                <div class="card-body">
+                    <p class="text-primary small mb-2 fw-semibold">Pet Health</p>
+                    <h5 class="card-title mb-3">Understanding Your Pet's Body Language: Signs of Illness</h5>
+                    <p class="card-text text-muted small mb-3">
+                        Lorem ipsum dolor sit amet consectetur. Arcu magna nam nibh ultricies. 
+                        Duis at congue ultrices at. Praesent risus risus tortor.
+                    </p>
+                    <p class="text-muted small mb-3">12 Oct, 2024</p>
+                    <a href="#" class="text-decoration-none d-inline-flex align-items-center">
+                        Read More 
+                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="ms-2">
+                            <path d="M1.25 8.25H15.25M15.25 8.25L8.25 1.25M15.25 8.25L8.25 15.25" stroke="#0082CF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+      
+        <div class="col-lg-4 col-md-6">
+            <div class="card h-100 border-0 ">
+                <img src="/images/blogs/blog-3.jpg" 
+                     class="card-img-top" alt="Blog Post" style="height: 250px; object-fit: cover;">
+                <div class="card-body">
+                    <p class="text-primary small mb-2 fw-semibold">Pet Health</p>
+                    <h5 class="card-title mb-3">Understanding Your Pet's Body Language: Signs of Illness</h5>
+                    <p class="card-text text-muted small mb-3">
+                        Lorem ipsum dolor sit amet consectetur. Arcu magna nam nibh ultricies. 
+                        Duis at congue ultrices at. Praesent risus risus tortor.
+                    </p>
+                    <p class="text-muted small mb-3">12 Oct, 2024</p>
+                    <a href="#" class="text-decoration-none d-inline-flex align-items-center">
+                        Read More 
+                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="ms-2">
+                            <path d="M1.25 8.25H15.25M15.25 8.25L8.25 1.25M15.25 8.25L8.25 15.25" stroke="#0082CF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+            <div class="card h-100 border-0 ">
+                <img src="/images/blogs/blog-4.jpg" 
+                     class="card-img-top" alt="Blog Post" style="height: 250px; object-fit: cover;">
+                <div class="card-body">
+                    <p class="text-primary small mb-2 fw-semibold">Pet Health</p>
+                    <h5 class="card-title mb-3">Understanding Your Pet's Body Language: Signs of Illness</h5>
+                    <p class="card-text text-muted small mb-3">
+                        Lorem ipsum dolor sit amet consectetur. Arcu magna nam nibh ultricies. 
+                        Duis at congue ultrices at. Praesent risus risus tortor.
+                    </p>
+                    <p class="text-muted small mb-3">12 Oct, 2024</p>
+                    <a href="#" class="text-decoration-none d-inline-flex align-items-center">
+                        Read More 
+                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="ms-2">
+                            <path d="M1.25 8.25H15.25M15.25 8.25L8.25 1.25M15.25 8.25L8.25 15.25" stroke="#0082CF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+       
+        <div class="col-lg-4 col-md-6">
+            <div class="card h-100 border-0 ">
+                <img src="/images/blogs/blog-5.jpg" 
+                     class="card-img-top" alt="Blog Post" style="height: 250px; object-fit: cover;">
+                <div class="card-body">
+                    <p class="text-primary small mb-2 fw-semibold">Pet Health</p>
+                    <h5 class="card-title mb-3">Understanding Your Pet's Body Language: Signs of Illness</h5>
+                    <p class="card-text text-muted small mb-3">
+                        Lorem ipsum dolor sit amet consectetur. Arcu magna nam nibh ultricies. 
+                        Duis at congue ultrices at. Praesent risus risus tortor.
+                    </p>
+                    <p class="text-muted small mb-3">12 Oct, 2024</p>
+                    <a href="#" class="text-decoration-none d-inline-flex align-items-center">
+                        Read More 
+                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="ms-2">
+                            <path d="M1.25 8.25H15.25M15.25 8.25L8.25 1.25M15.25 8.25L8.25 15.25" stroke="#0082CF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+     
+        <div class="col-lg-4 col-md-6">
+            <div class="card h-100 border-0 ">
+                <img src="/images/blogs/blog-6.jpg" 
+                     class="card-img-top" alt="Blog Post" style="height: 250px; object-fit: cover;">
+                <div class="card-body">
+                    <p class="text-primary small mb-2 fw-semibold">Pet Health</p>
+                    <h5 class="card-title mb-3">Understanding Your Pet's Body Language: Signs of Illness</h5>
+                    <p class="card-text text-muted small mb-3">
+                        Lorem ipsum dolor sit amet consectetur. Arcu magna nam nibh ultricies. 
+                        Duis at congue ultrices at. Praesent risus risus tortor.
+                    </p>
+                    <p class="text-muted small mb-3">12 Oct, 2024</p>
+                    <a href="#" class="text-decoration-none d-inline-flex align-items-center">
+                        Read More 
+                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="ms-2">
+                            <path d="M1.25 8.25H15.25M15.25 8.25L8.25 1.25M15.25 8.25L8.25 15.25" stroke="#0082CF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+            <div class="card h-100 border-0 ">
+                <img src="/images/blogs/blog-7.jpg" 
+                     class="card-img-top" alt="Blog Post" style="height: 250px; object-fit: cover;">
+                <div class="card-body">
+                    <p class="text-primary small mb-2 fw-semibold">Pet Health</p>
+                    <h5 class="card-title mb-3">Understanding Your Pet's Body Language: Signs of Illness</h5>
+                    <p class="card-text text-muted small mb-3">
+                        Lorem ipsum dolor sit amet consectetur. Arcu magna nam nibh ultricies. 
+                        Duis at congue ultrices at. Praesent risus risus tortor.
+                    </p>
+                    <p class="text-muted small mb-3">12 Oct, 2024</p>
+                    <a href="#" class="text-decoration-none d-inline-flex align-items-center">
+                        Read More 
+                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="ms-2">
+                            <path d="M1.25 8.25H15.25M15.25 8.25L8.25 1.25M15.25 8.25L8.25 15.25" stroke="#0082CF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+            <div class="card h-100 border-0 ">
+                <img src="/images/blogs/blog-8.jpg" 
+                     class="card-img-top" alt="Blog Post" style="height: 250px; object-fit: cover;">
+                <div class="card-body">
+                    <p class="text-primary small mb-2 fw-semibold">Pet Health</p>
+                    <h5 class="card-title mb-3">Understanding Your Pet's Body Language: Signs of Illness</h5>
+                    <p class="card-text text-muted small mb-3">
+                        Lorem ipsum dolor sit amet consectetur. Arcu magna nam nibh ultricies. 
+                        Duis at congue ultrices at. Praesent risus risus tortor.
+                    </p>
+                    <p class="text-muted small mb-3">12 Oct, 2024</p>
+                    <a href="#" class="text-decoration-none d-inline-flex align-items-center">
+                        Read More 
+                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="ms-2">
+                            <path d="M1.25 8.25H15.25M15.25 8.25L8.25 1.25M15.25 8.25L8.25 15.25" stroke="#0082CF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+            <div class="card h-100 border-0 ">
+                <img src="/images/blogs/blog-9.jpg" 
+                     class="card-img-top" alt="Blog Post" style="height: 250px; object-fit: cover;">
+                <div class="card-body">
+                    <p class="text-primary small mb-2 fw-semibold">Pet Health</p>
+                    <h5 class="card-title mb-3">Understanding Your Pet's Body Language: Signs of Illness</h5>
+                    <p class="card-text text-muted small mb-3">
+                        Lorem ipsum dolor sit amet consectetur. Arcu magna nam nibh ultricies. 
+                        Duis at congue ultrices at. Praesent risus risus tortor.
+                    </p>
+                    <p class="text-muted small mb-3">12 Oct, 2024</p>
+                    <a href="#" class="text-decoration-none d-inline-flex align-items-center">
+                        Read More 
+                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="ms-2">
+                            <path d="M1.25 8.25H15.25M15.25 8.25L8.25 1.25M15.25 8.25L8.25 15.25" stroke="#0082CF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container text-center mb-5">
+    <button class="btn btn-primary btn-lg px-5">View More</button>
 </div>
 @endsection
