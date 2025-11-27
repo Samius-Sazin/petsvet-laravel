daff4e472b1230ff81f6febfb5ee40cc1ab22e32
sabuj494 <sabuj494k@gmail.com>
2025-11-27 22:19:42 +0600

    [fix] article likes

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- favicon png format --}}
     <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png" style="border-radius: 50%">
