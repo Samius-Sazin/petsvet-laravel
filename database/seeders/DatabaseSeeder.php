a6f21f254ac781b33dd05c1bc4b62a713b9c3416
sabuj494 <sabuj494k@gmail.com>
2025-11-28 03:00:21 +0600

    [merge] feat-backend/qna

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             ProductsTableSeeder::class,
             UsersTableSeeder::class,
             ArticlesTableSeeder::class,
+            QnaSeeder::class
         ]);
     }
 
